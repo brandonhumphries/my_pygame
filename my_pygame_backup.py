a0c739a9d7418008117b9de0fc830eb22c323537
Brandon Humphries <brandonhumphries@Brandons-MacBook-Air.local>
2018-07-10 14:36:38 -0400

    added Monster class, added timed, random movement

--- my_pygame_backup.py
+++ my_pygame_backup.py
@@ -1,7 +1,38 @@
 import pygame
+import random
+
+class Monster(object):
+    def __init__(self):
+        self.x = random.randint(0, 510)
+        self.y = random.randint(0, 480)
+
+    def move_north(self):
+        self.y -= 5
+        if self.y < 0:
+            self.y = 480
+        # return self.y
+
+    def move_east(self):
+        self.x += 5
+        if self.x > 510:
+            self.x = 0
+        # return self.x
+
+    def move_south(self):
+        self.y += 5
+        if self.y > 480:
+            self.y = 0
+        # return self.y
+
+    def move_west(self):
+        self.x -= 5
+        if self.x < 0:
+            self.x = 510
+        # return self.x
+        
 
 def main():
-    width = 500
+    width = 510
     height = 480
     blue_color = (97, 159, 182)
 
@@ -12,6 +43,14 @@ def main():
 
     # Game initialization
     background_image = pygame.image.load('images/background.png').convert_alpha()
+    hero_image = pygame.image.load('images/hero.png').convert_alpha()
+    monster_image = pygame.image.load('images/monster.png').convert_alpha()
+
+    orc = Monster()
+
+    # monster_x = random.randint(0, 510)
+    # monster_y = random.randint(0, 480)
+    change_dir_countdown = 120
     stop_game = False
     while not stop_game:
         for event in pygame.event.get():
@@ -23,12 +62,28 @@ def main():
 
 
         # Game logic
+        change_dir_countdown -= 1
+        if change_dir_countdown == 0:
+            change_dir_countdown = 120
+            direction_number = random.randint(0, 3)
+            if direction_number == 0:
+                orc.move_north()
+            if direction_number == 1:
+                orc.move_east()
+            if direction_number == 2:
+                orc.move_south()
+            if direction_number == 3:
+                orc.move_west()
+        
+        
 
         # Draw background
         screen.fill(blue_color)
 
         # Game display
         screen.blit(background_image, (0, 0))
+        screen.blit(hero_image, (255, 240))
+        screen.blit(monster_image, (orc.x, orc.y))
         pygame.display.update()
         clock.tick(60)
 
